0fc8037b6761c0f00d8f5d60c8c7a523affb7ff9
ismsondam <1dam.iyilmaz@institutoserpis.org>
2019-01-28 09:54:04 +0100

    Actividad saludar

--- AndroidStudio/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,19 +3,23 @@ package com.example.myfirstapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText textNombre;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = ;
+        textNombre = findViewById(R.id.textNombre);
+        textView = findViewById(R.id.TextView1);
     }
 
     public void saludar(View view) {
-
+        textView.setText("Hola, " + textNombre.getText());
     }
 }
